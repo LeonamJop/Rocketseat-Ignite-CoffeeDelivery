235d5a5daca8bfe7897a728beae1495831f7cfe0
Leonam Silva <leonam.murilo@gmail.com>
2023-02-08 22:29:11 -0300

    Alterando tipagem do preço para number

--- src/pages/Home/components/ProductList/index.tsx
+++ src/pages/Home/components/ProductList/index.tsx
@@ -25,7 +25,7 @@ const cards = [
     tags: ['TRADICIONAL'],
     name: 'Expresso Tradicional',
     description: 'O tradicional café feito com água quente e grãos moídos',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 2,
@@ -33,7 +33,7 @@ const cards = [
     tags: ['TRADICIONAL'],
     name: 'Expresso Americano',
     description: 'Expresso diluído, menos intenso que o tradicional',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 3,
@@ -41,7 +41,7 @@ const cards = [
     tags: ['TRADICIONAL'],
     name: 'Expresso Cremoso',
     description: 'Café expresso tradicional com espuma cremosa',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 4,
@@ -49,7 +49,7 @@ const cards = [
     tags: ['TRADICIONAL', 'GELADO'],
     name: 'Expresso Gelado',
     description: 'Bebida preparada com café expresso e cubos de gelo',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 5,
@@ -57,7 +57,7 @@ const cards = [
     tags: ['TRADICIONAL', 'COM LEITE'],
     name: 'Café com Leite',
     description: 'Meio a meio de expresso tradicional com leite vaporizado',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 6,
@@ -66,7 +66,7 @@ const cards = [
     name: 'Latte',
     description:
       'Uma dose de café expresso com o dobro de leite e espuma cremosa',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 7,
@@ -75,7 +75,7 @@ const cards = [
     name: 'Capuccino',
     description:
       'Bebida com canela feita de doses iguais de café, leite e espuma',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 8,
@@ -84,7 +84,7 @@ const cards = [
     name: 'Macchiato',
     description:
       'Café expresso misturado com um pouco de leite quente e espuma',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 9,
@@ -92,7 +92,7 @@ const cards = [
     tags: ['TRADICIONAL', 'COM LEITE'],
     name: 'Mocaccino',
     description: 'Café expresso com calda de chocolate, pouco leite e espuma',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 10,
@@ -100,7 +100,7 @@ const cards = [
     tags: ['ESPECIAL', 'COM LEITE'],
     name: 'Chocolate Quente',
     description: 'Bebida feita com chocolate dissolvido no leite quente e café',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 11,
@@ -109,7 +109,7 @@ const cards = [
     name: 'Cubano',
     description:
       'Drink gelado de café expresso com rum, creme de leite e hortelã',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 12,
@@ -117,7 +117,7 @@ const cards = [
     tags: ['ESPECIAL'],
     name: 'Havaiano',
     description: 'Bebida adocicada preparada com café e leite de coco',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 13,
@@ -125,7 +125,7 @@ const cards = [
     tags: ['ESPECIAL'],
     name: 'Árabe',
     description: 'Bebida preparada com grãos de café árabe e especiarias',
-    price: '9,90',
+    price: 9.9,
   },
   {
     id: 14,
@@ -133,7 +133,7 @@ const cards = [
     tags: ['ESPECIAL', 'ALCOÓLICO'],
     name: 'Irlandês',
     description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
-    price: '9,90',
+    price: 9.9,
   },
 ]
 
